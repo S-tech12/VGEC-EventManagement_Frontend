2b11ea656d3c02f369c43b4d56ec10b9e79c47f9
S-tech12 <119032842+S-tech12@users.noreply.github.com>
2025-06-05 15:11:50 +0530

    Update (4)RejectedEvent.js

--- Profile_page_for_EventHoster/(4)RejectedEvent.js
+++ Profile_page_for_EventHoster/(4)RejectedEvent.js
@@ -18,7 +18,7 @@ async function FillTable() {
     }
 
     try {
-        const response = await fetch("http://localhost:3000/getEventData", {
+        const response = await fetch("https://vgec-eventmanagement-backend.onrender.com/getEventData", {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
